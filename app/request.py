f197d9cfa1747a5994efd079768000ab7ac58070
iankurao <kuraoian@gmail.com>
2019-05-20 09:58:01 +0300

    get Api keys and urls

--- app/request.py
+++ app/request.py
@@ -3,3 +3,7 @@ import urllib.request, json
 from .models import source,topHeadlines, everything
 
 api_key = app.config['NEWS_API_KEY']
+source_url = app.config['SOURCES_API_BASE_URL']
+top_headlines_url = app.config['TOP_HEADLINES_BASE_URL']
+everything_url = app.config['EVERYTHING_BASE_URL']
+
