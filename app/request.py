b9ca72ea97d408b6c3872acefa5ed01ae6c6fe91
iankurao <kuraoian@gmail.com>
2019-05-20 10:21:41 +0300

    create a function that process the source results and tranforms it to a list of objects

--- app/request.py
+++ app/request.py
@@ -22,3 +22,22 @@ def get_sources():
             source_results_list = get_sources_response['sources']
             source_results = process_source_results(source_results_list)
     return source_results
+
+
+def process_source_results(source_list):
+    source_results = []
+
+    for source_item in source_list:
+        id = source_item.get('id')
+        name = source_item.get('name')
+        description = source_item.get('description')
+        url = source_item.get('url')
+        category = source_item.get('category')
+        language = source_item.get('language')
+        country = source_item.get('country')
+
+        source_object = Source( id, name, description, url, category, language, country)
+        source_results.append(source_object)
+
+    return source_results
+    
