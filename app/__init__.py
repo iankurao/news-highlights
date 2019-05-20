f0cff0434ad7722caac392b44695952915d839a5
iankurao <kuraoian@gmail.com>
2019-05-20 13:48:49 +0300

    create a blue print

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,29 @@
 from flask import Flask
-from .config import DevConfig
+from config import config_options
 from flask_bootstrap import Bootstrap
-app = Flask(__name__, instance_relative_config = True)
 
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+bootstrap = Bootstrap()
 
-bootstrap = Bootstrap(app)
+def create_app(config_name):
 
-from app import views 
+   app = Flask(__name__, instance_relative_config = True)
+   
+   # Creating the app configurations
+   app.config.from_object(config_options[config_name])
+
+   # Initializing flask extensions
+   bootstrap.init_app(app)
+
+   
+   #register blueprint
+   from .main import main as main_blueprint
+   app.register_blueprint(main_blueprint)
+
+   #setting config
+   from .request import configure_request
+   configure_request(app)
+   
+   # Will add the views and forms
+
+   return app
+
