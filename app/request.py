35c11699096ed851731754d749d8eaeeff50c855
iankurao <kuraoian@gmail.com>
2019-05-20 10:17:41 +0300

    create a function that join json response to url request

--- app/request.py
+++ app/request.py
@@ -2,8 +2,23 @@ from app import app
 import urllib.request, json
 from .models import source,topHeadlines, everything
 
+Source = source.Source
+
 api_key = app.config['NEWS_API_KEY']
 source_url = app.config['SOURCES_API_BASE_URL']
 top_headlines_url = app.config['TOP_HEADLINES_BASE_URL']
 everything_url = app.config['EVERYTHING_BASE_URL']
 
+def get_sources():
+    get_sources_url = source_url.format(api_key)
+
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+
+        source_results = None
+
+        if get_sources_response['sources']:
+            source_results_list = get_sources_response['sources']
+            source_results = process_source_results(source_results_list)
+    return source_results
